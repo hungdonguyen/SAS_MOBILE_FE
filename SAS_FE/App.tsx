d54a8bf1f18836187805890a5c6b8c8ad3e8f8d6
Nguyễn Việt Hưng <hung.nguyenviet.cit22@eiu.edu.vn>
2026-08-19 23:21:55 +0700

    screen bug fix

--- SAS_FE/App.tsx
+++ SAS_FE/App.tsx
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
 
